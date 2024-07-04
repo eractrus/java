4c64befd87ab2b0076a9cab308c0e1c207684309
Eractrus de Judá <eractrus@gmail.com>
2024-07-04 11:53:54 -0300

    finalizando a matéria de anatomia de classe em JAVA

--- java/anatomia-classes-java/src/MinhaClasse.java
+++ java/anatomia-classes-java/src/MinhaClasse.java
@@ -1,3 +1,29 @@
+
+//class geral
 public class MinhaClasse {
+    // classe default principal
+    public static void main(String[] args) {
+        // lógica
+        System.out.println("Iniciando em JAVA");
+
+        // variável
+        // Declaração de variável - tipo | nome | valor(opcional);
+        String BR = "Brasil";
+
+        // para uma variável ser imutável add a restrição final
+        final String nome = "Desenvolvedor";
+
+        System.out.println(nome);
+
+        //chamando o método somar
+        System.out.println(somar(1, 1));
+    }
     
+    // método
+    // Declaração de método - tipoRetorno | nome (no infinitivo) | parâmentro
+    public static int somar(int a, int b) {
+        return a + b;
+    }
+
+    //JAVA BEANS
 }
