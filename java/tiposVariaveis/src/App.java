508505bf9431ab68f4cfd96c5cc98a89ad03f070
Eractrus de Judá <eractrus@gmail.com>
2024-07-04 12:59:28 -0300

    finalizado a matéria de tipos e variáveis

--- java/tiposVariaveis/src/App.java
+++ java/tiposVariaveis/src/App.java
@@ -1,5 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        // Tipos de dados básicos | tipos primitivos
+
+        //int, byte,short, long, float, double, boolean, char - armazeado diretamente na pilha de memória
+        // floar, double
+
+        byte idade = 28;
+        short ano = 2024;
+        int cep = 250886443;
+        long cpf = 12345678944L;// esse tipo precisa terminar com 'L' no fina do valor
+        float pi = 3.14f;// esse tipo precisa terminar com 'F' no fina do valor
+        double salario = 2500;
     }
 }
