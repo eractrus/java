6ae5944dbadb23a44b9f6c09c1e5f3bb6f977f1c
Eractrus de Judá <eractrus@gmail.com>
2024-07-08 11:13:09 -0300

    Operadores finalizado

---
+++ java/operadores/src/App.java
@@ -0,0 +1,33 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        //OPERADORES
+
+        //OPERADORES ARITIMÉTICOS
+            // São utilizados para realizar operações matemáticas = +, -, *, /
+
+            System.out.println(1+1);
+
+        //OPERADORES UNÁRIOS
+            //São operadores que sua utilização é feita com outro operador aritimético ex: incrementar ou decrementar valores ++ | -- | ! negação
+
+            System.out.println(!true);
+
+            int numero = 0;
+            System.out.println(++numero);
+            System.out.println(--numero);
+            
+        //OPERADORES RELACIONAIS
+            //São utilizados para 
+
+        //OPERADORES LÓGICOS
+            // && , ||
+
+        //OPERADORES TERNÁRIOS
+            //Expressão condicional ? condição verdadeira : condição falsa
+
+            int num = 10;
+            boolean result = num == 5 ? true : false ;
+            System.out.println(result);
+    }
+}
